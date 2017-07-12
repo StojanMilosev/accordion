5562080881ac77e88c72145d919e220b366b6fd3
stojan.milosev <stojan.milosev@yahoo.com>
2017-07-12 14:58:00 +0200

    added JS funcionality

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-//get accordion element through class name
-var accordion  = document.getElementsByClassName("accordion");
+//preuzimanje elementa sa klasom "accordion"
+var acc = document.getElementsByClassName("accordion");
 
-for(var i =0;i<accordion.length;i++){
-    accordion[i].onclick = function(){
+for (var i = 0; i < acc.length; i++) {
+    //menjane osobina elementa prilikom klika
+    acc[i].onclick = function(){
+        //menjane postojanja klase "active" u atributu class="" elementa 
         this.classList.toggle("active");
+        //menjane osobina panel elementa koji je sledece dete u odnosu na element accordion
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            //ako je panel prikazan menjanjem osobine display ga sakrivamo
+            panel.style.display = "none";
+        } else {
+            //ako je panel sakriven menjanjem osobine display ga prikazujemo
+            panel.style.display = "block";
+        }
     }
-} 
+}
